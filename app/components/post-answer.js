df6dc0142f66bc9306a2073241bbc4a6fcd628c7
HipsterBrown <headhipster@hipsterbrown.com>
2014-06-22 11:49:20 -0400

    basic heading animations and intro along with styles for post-answer cards

---
+++ app/components/post-answer.js
@@ -0,0 +1,7 @@
+var PostAnswerComponent = Ember.Component.extend({
+  tagName: 'article',
+  classNames: ['answer', 'card', 'container', 'thin']
+
+});
+
+export default PostAnswerComponent;
---
+++ tests/unit/components/post-answer-tests.js
@@ -0,0 +1,14 @@
+import { test, moduleForComponent } from 'ember-qunit';
+
+var component;
+
+moduleForComponent('post-answer', 'Unit - Post answer component',
+                   {
+  setup: function() {
+    component = this.subject();
+  }
+});
+
+test('Write a test here', function() {
+  ok( false, 'Implement Me' );
+});
