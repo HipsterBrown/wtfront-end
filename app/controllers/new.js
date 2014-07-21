1e58498ecf92c42537f42ef933223cae9c5033f6
HipsterBrown <headhipster@hipsterbrown.com>
2014-07-20 21:59:46 -0400

    new post controller edits and attempts to make FirebaseSimpleLogin work with ember-cli

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -1,3 +1,29 @@
-export default Ember.ObjectController.extend({
-  isLoggedIn: true
+//import Firebase from "../adapters/application";
+
+var NewController = Ember.Controller.extend({
+	isLoggedIn: false,
+	answerText: "",
+	user: "",
+	output: Ember.computed.oneWay("answerText"),
+	auth: null,/*
+	makeAuth: function() {
+		var ref = this.get('container').lookup('adapter:application').get('firebase'),
+		auth = new window.FirebaseSimpleLogin(ref, function(error, user) {
+			if (!error) {
+					this.set('user', user.id);
+					this.set('isLoggedIn', true);
+					window.alert('Logged In!');
+				} else {
+					window.alert(error);
+				}
+			});
+		this.set('auth', auth);
+	}.on('init'),*/
+	actions: {
+		login: function() {
+			this.set('isLoggedIn', true);
+		}
+	}
 });
+
+export default NewController;
